407043d56c46a43e86dd397c939bf94c24c8be6a
AyyJimmehh <steveparakal@gmail.com>
2025-06-23 16:50:03 +0300

    Fix: improved error handling in predict endpoint

--- app/utils/security.py
+++ app/utils/security.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+api_key_header = APIKeyHeader(name="api-key", auto_error=False)
 
 def get_api_key(user_key: str = Security(api_key_header)):
     if user_key != API_KEY:
